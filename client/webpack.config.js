6cf00a0445206979ed3406f5ed45ee09b04f39ce
janae.wel <janae.wel@gmail.com>
2022-10-18 17:29:34 -0400

    webpack config done

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,12 +18,49 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HTMLWebpackPlugin({
+        template: "./index.html",
+        title: 'text-editor'
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js'
+      }),
+      new WebpackPwaManifest({
+        name: 'Text Editor',
+        short_name: 'Notes',
+        description: 'Keep track of important notes!',
+        background_color: '#7eb4e2',
+        theme_color: '#7eb4e2',
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {src: path.resolve('src/images/logo.png'),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join('assets', 'icons'),
+        }]
+      })
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        },
       ],
     },
   };
